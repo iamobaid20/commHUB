255a70d6e6a95e2fa99c4a6f94b8dc0dc7454262
iamobaid20 <khanobaid20@gmail.com>
2024-05-07 00:59:48 +0530

    project completed and running

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,52 +1,52 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
-export const sendMessage = async (req,res)=>{
-   try{
-    const {message} = req.body;
-    const {id: receiverId} = req.params; //jo parameter value aayi hai wo reciever id hai aur humne iss syntax se id ko rename karke receiverid kar diya
-    const senderId = req.user._id // we will acces data base user because we have added the data base user in the protectRoute middleware last mein
+export const sendMessage = async (req, res) => {
+	try {
+		const { message } = req.body;
+		const { id: receiverId } = req.params;
+		const senderId = req.user._id;
 
-    //finding if there is any convo between senderId and receiverId
-    let conversation = await Conversation.findOne({
-        participants : {$all : [senderId, receiverId]}, // we are not putting message becuase in the model we declare default value as [] empty array
-    });
+		let conversation = await Conversation.findOne({
+			participants: { $all: [senderId, receiverId] },
+		});
 
-    //if there is no conversation then we will create one conversation
-    if(!conversation){
-        conversation = await Conversation.create({
-            participants: [senderId, receiverId],
-        });
-    }
+		if (!conversation) {
+			conversation = await Conversation.create({
+				participants: [senderId, receiverId],
+			});
+		}
 
-    // if there is a message
+		const newMessage = new Message({
+			senderId,
+			receiverId,
+			message,
+		});
 
-    const newMessage = new Message({
-        senderId,
-        receiverId,
-        message,
-    });
+		if (newMessage) {
+			conversation.messages.push(newMessage._id);
+		}
 
-    if(newMessage){
-        conversation.messages.push(newMessage._id);
-    }
-        // await conversation.save();
+		// await conversation.save();
 		// await newMessage.save();
 
 		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
-        // SOCKET IO FUNCTIONALITY WILL GO HERE
+		// SOCKET IO FUNCTIONALITY WILL GO HERE
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
-    res.status(201).json(newMessage);
-
-   }
-   catch(error)
-   {
-    console.log("Error in sendMesage controller",error.message)
-    res.status(500).json({error:"Internal server error"})
-   }
-}
+		res.status(201).json(newMessage);
+	} catch (error) {
+		console.log("Error in sendMessage controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
 
 export const getMessages = async (req, res) => {
 	try {
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"; //package import
 import dotenv from "dotenv";  //package import
 import cookieParser from "cookie-parser";// this is to parse the cookie and give us the jwt token from the cookie
@@ -7,9 +8,11 @@ import messageRoutes from "./routes/message.routes.js"; //file import
 import userRoutes from "./routes/user.routes.js"; //file import
 
 import connectToMongoDB from "./db/connectToMongoDB.js"; //file import
-
-const app = express();
+import { app, server } from "./socket/socket.js";
+//const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,10 +23,16 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+ app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 //app.get("/",(req,res)=>{res.send("WELCOME, obaid")});
 
 
-app.listen(PORT, ()=> {
+server.listen(PORT, ()=> {
     connectToMongoDB();
     console.log(`server is running on port ${PORT}`);
 }) 
